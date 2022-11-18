f08a468d85f9b96bd194ae2c1269d2189af192ee
Leo Batan <leobatan@Leos-MacBook-Pro.local>
2022-11-17 22:26:55 -0500

    Update auth domain

--- src/index.js
+++ src/index.js
@@ -27,8 +27,7 @@ import {
 
 const firebaseConfig = {
   apiKey: "AIzaSyDgLEo4-GeXRZkLPXJ40Hvns6Gd-8qdi18",
-  // authDomain: "fire-giftr-1b0b0.firebaseapp.com",
-  authDomain: "auth.github.com",
+  authDomain: "fire-giftr-1b0b0.firebaseapp.com",
   projectId: "fire-giftr-1b0b0",
   storageBucket: "fire-giftr-1b0b0.appspot.com",
   messagingSenderId: "314280737166",
